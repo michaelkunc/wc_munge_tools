04b569f6c316fbd6cc2fe4ba3a61744320d5088b
michaelkunc <mjkunc@gmail.com>
2016-01-06 10:07:24 -0600

    initial commit

---
+++ wc.py
@@ -0,0 +1,17 @@
+
+
+def open_and_split(filepath):
+	with open(filepath) as f:
+		lines = f.read().splitlines()
+		return lines
+
+
+def clean_lines(user_list, character, include_exclude):
+	if include_exclude == 'include':
+		lines = [l for l in user_list if character in l]
+		return lines
+	elif include_exclude == 'exclude':
+		lines = [l for l in user_list if character not in l]
+		return lines
+	else:
+		raise ValueError("Please enter 'include' or 'exclude' for the final argument")
---
+++ wc_test.py
@@ -0,0 +1,7 @@
+import unittest
+
+from wc import open_and_split
+
+class Open_and_split_Tests(unittest.TestCase):
+
+	def test_open_file
